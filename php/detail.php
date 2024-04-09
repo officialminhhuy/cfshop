c91446a133bd906683cc4a4194569429d1355e39
Nguyễn Minh Huy <quilivh11@gmail.com>
2024-04-09 09:42:46 +0700

    master

--- php/detail.php
+++ php/detail.php
@@ -22,81 +22,80 @@
 
 
         ?>
-        <div class="detailsz">
-            <a id="detailsz">
+                    <div class="detailsz">
+                        <a id="detailsz">
 
-                <form method="post" action="update.php" enctype="multipart/form-data">
-                    <img src="/assets/images/<?php echo $row["image"]; ?>" alt="" class="edimg"><br>
-                    <p id="fontedit" style="display: none;">
-                        ID
-                        <input class="sizeedit" name="id" type="text" value="<?php echo $productId ?>">
-                    </p>
-                    <p id="fontedit">
-                        Name:
-                        <input class="sizeedit" name="proname" type="text" value="<?php echo $row['PName']; ?>"><br>
-                    </p>
+                            <form method="post" action="update.php" enctype="multipart/form-data">
+                                <img src="/assets/images/<?php echo $row["image"]; ?>" alt="" class="edimg"><br>
+                                <p id="fontedit" style="display: none;">
+                                    ID
+                                    <input class="sizeedit" name="id" type="text" value="<?php echo $productId ?>">
+                                </p>
+                                <p id="fontedit">
+                                    Name:
+                                    <input class="sizeedit" name="proname" type="text" value="<?php echo $row['PName']; ?>"><br>
+                                </p>
 
-                    <p id="fontedit">
-                        Material:
-                        <input class="sizeedit" name="material" type="text" value="<?php echo $row['Material']; ?>">
-                    </p>
-                    <p id="fontedit">
-                        Price:
-                        <input class="sizeedit" name="prices" value="<?php echo $row['prices']; ?>">
-                    </p>
-                    <p id="fontedit">
-                        Available:
-                        <input class="sizeedit" name="validpro" type="text" value="<?php echo $row["validproduct"] ?>">
-                    </p>
-                    <p id="fontedit" style="text-transform: none;">
-                        New Image:
-                        <input type="file" name="imgs" accept="image/*">
-                        (Skip if not update)
-                    </p>
-                    <button class="btn updateproduct" onclick="addCartClicked()" type="submit">Update</button>
-                </form>
-                <div id="back">
-                    <a1 class="btn back" onclick="hidePopup();closePopupHandler();">Back</a1>
-                </div>
+                                <p id="fontedit">
+                                    Material:
+                                    <input class="sizeedit" name="material" type="text" value="<?php echo $row['Material']; ?>">
+                                </p>
+                                <p id="fontedit">
+                                    Price:
+                                    <input class="sizeedit" name="prices" value="<?php echo $row['prices']; ?>">
+                                </p>
+                                <p id="fontedit">
+                                    Available:
+                                    <input class="sizeedit" name="validpro" type="text" value="<?php echo $row["validproduct"] ?>">
+                                </p>
+                                <p id="fontedit" style="text-transform: none;">
+                                    New Image:
+                                    <input type="file" name="imgs" accept="image/*">
+                                    (Skip if not update)
+                                </p>
+                                <button class="btn updateproduct" onclick="addCartClicked()" type="submit">Update</button>
+                            </form>
+                            <div id="back">
+                                <a1 class="btn back" onclick="hidePopup();closePopupHandler();">Back</a1>
+                            </div>
 
-                <a3 class="idpro" style="display: none;"> <?php echo $productId; ?></a3>
-            </a>
-        </div>
-        <?php
+                            <a3 class="idpro" style="display: none;"> <?php echo $productId; ?></a3>
+                        </a>
+                    </div>
+                <?php
 
                 } else {
 
                 ?>
-        <div class="detailsz">
-            <a id="detailsz">
+                    <div class="detailsz">
+                        <a id="detailsz">
 
-                <p id="fontedit" style="display: none;">
-                    ID
-                    <input class="sizeedit" name="id" type="text" value="<?php echo $productId ?>">
-                </p>
-                <img src="/assets/images/<?php echo $row["image"]; ?>" alt="" class="dtimg"><br>
-                <h1 id="dttext"><?php echo $row['PName']; ?></h1><br>
-                <!-- <p>Available:<?php echo $row["validproduct"] ?> </p>; -->
-                <p id="material"><?php echo $row['Material']; ?></p>
-                <p id="price">Price: <?php echo $row['prices']; ?>VND</p>
-                <p id="fontedit" style="display: none;">
-                    Price:
-                    <input class="sizeedit" name="price" type="text" value="<?php echo $row['prices']; ?>">
-                </p>
+                            <p id="fontedit" style="display: none;">
+                                ID
+                                <input class="sizeedit" name="id" type="text" value="<?php echo $productId ?>">
+                            </p>
+                            <img src="/assets/images/<?php echo $row["image"]; ?>" alt="" class="dtimg"><br>
+                            <h1 id="dttext"><?php echo $row['PName']; ?></h1><br>
+                            <!-- <p>Available:<?php echo $row["validproduct"] ?> </p>; -->
+                            <p id="material"><?php echo $row['Material']; ?></p>
+                            <p id="price">Price: <?php echo $row['prices']; ?>VND</p>
+                            <p id="fontedit" style="display: none;">
+                                Price:
+                                <input class="sizeedit" name="price" type="text" value="<?php echo $row['prices']; ?>">
+                            </p>
 
-                <form>
-                    <button class="btn add-cart" data-product-add=" <?php echo $productId ?> "
-                        onclick="hidePopup();closePopupHandler();redirectCart();">Add
-                        to Cart</button>
-                </form>
-                <div id="back">
-                    <button class="btn back" onclick="hidePopup();closePopupHandler();">Back</button>
+                            <form>
+                                <button class="btn add-cart" data-product-add=" <?php echo $productId ?> " onclick="redirectCart();">Add
+                                    to Cart</button>
+                            </form>
+                            <div id="back">
+                                <button class="btn back" onclick="hidePopup();closePopupHandler();">Back</button>
 
-                </div>
-                <a3 class="idpro" style="display: none;"> <?php echo $productId; ?></a3>
+                            </div>
+                            <a3 class="idpro" style="display: none;"> <?php echo $productId; ?></a3>
 
-            </a>
-        </div>
+                        </a>
+                    </div>
         <?php
                 }
             } else {
@@ -109,10 +108,13 @@
     </div>
 </div>
 <script>
-function redirectCart() {
-    if (!"<?php echo isset($_SESSION["username"]) ? $_SESSION["username"] : '' ?>") {
-        alert("You are not logged in. Please log into your account and try again.");
-        window.location.href = "login.php";
+    function redirectCart() {
+        if (!<?php echo isset($_SESSION["username"]) ? 'true' : 'false' ?>) {
+            alert("You are not logged in. Please log into your account and try again.");
+            window.location.href = "login.php";
+        } else {
+            hidePopup();
+            closePopupHandler();
+        }
     }
-}
 </script>
